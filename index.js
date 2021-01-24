2b7a0cf5fd8bad1fbd412c0989b00e071de709de
S M AL AMIN <76505639+alamin93@users.noreply.github.com>
2021-01-24 11:38:06 +0600

    Add files via upload
    use evenlistner with multipur element

---
+++ index.js
@@ -0,0 +1,12 @@
+//evenlistenr with multipul element
+var len =document.querySelectorAll(".myButton").length;
+for(var i=0;i<len;i++){
+    document.querySelectorAll(".myButton")[i].addEventListener("click",function(){
+        var text = this.innerHTML;
+        document.querySelector(".head1").innerHTML= text +" is clicked";
+    });
+
+}
+
+
+
